d8cbde7364c85e4b1b05c16ffc8eb9f1d684d299
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-03-19 23:40:43 +0300

    refactor: server seperated from the client

--- client/src/constants/constants.ts
+++
@@ -1,9 +0,0 @@
-let URI: string;
-
-if (process.env.NODE_ENV === "development") {
-  URI = "http://localhost:8000/graphql";
-} else {
-  URI = "some_base_production_url/graphql";
-}
-
-export { URI };
--- client/src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Home: React.FC = (): JSX.Element => {
-  return <div>Home</div>;
-};
