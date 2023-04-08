ad31b93624383e56bb406dce6c63a325df2fa962
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-04-08 19:08:44 +0300

    feat: find files by folderId and subfolderId

--- src/graphql/resolvers/file/queries.ts
+++ src/graphql/resolvers/file/queries.ts
@@ -1,6 +1,27 @@
+import { AppError } from "../../../utils/error";
+import { File } from "../../../db/models";
+
+const file = new File();
+
 const fileQueries = {
-  // authors: async (_, args:any) => {},
-  // author: async (_, args:any) => {},
+  findFileByFolderId: async (_: any, args: any) => {
+    const folderId = args.id;
+    if (!folderId) {
+      new AppError("please supply folder Id", 400).badUserInput();
+    }
+    const savedFiles = await file.findByFolderId(folderId);
+
+    return savedFiles;
+  },
+  findFileBySubFolderId: async (_: any, args: any) => {
+    const subFolderId = args.id;
+    if (!subFolderId) {
+      new AppError("please supply folder Id", 400).badUserInput();
+    }
+    const savedFiles = await file.findBySubFolderId(subFolderId);
+
+    return savedFiles;
+  },
 };
 
 export default fileQueries;
